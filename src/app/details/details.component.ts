9d8bb71fe4487b202c937634dd4f1daca34eeb4f
Bhavyai2e <bhavya.paliwal@i2econsulting.com>
2024-10-28 17:17:16 +0530

    New Changes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,47 +1,48 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { HousingService } from '../housing.service';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {HousingService} from '../housing.service';
 import { HousingLocation } from '../housing-location';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
-    <img class="listing-photo" [src] = "housingLocation?.photo">
-    <section class="listing-description">
-    <h2 class="listing-heading"> {{housingLocation?.name}}</h2>
-    <p class="listing- location" > {{housingLocation?.city}} , {{housingLocation?.state}}
-    </section>
-    <section class="listing-features">
-    <h2 class="section-heading"> About this Housing Location</h2>
-    <ul>
+      <img
+        class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{ housingLocation?.name }}"
+        crossorigin
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
           <li>Units available: {{ housingLocation?.availableUnits }}</li>
           <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+          <li>Does this location have laundry: {{ housingLocation?.laundary }}</li>
         </ul>
-    </section>
-    <section class="listing-apply">
-      <h2 class="section-heading">Apply now to live here</h2>
-      <form [formGroup]="applyForm" (submit)="submitApplication()">
-      <label for ="first-name">First name </label>
-      <input id="first-name" type="text" formControlName="firstName">
-
-      <label id ="last-name">last name </label>
-      <input id="last-name" type="text" formControlName="lastName">
-
-      <label for ="email">Email</label>
-      <input id="email" type="email" formControlName="email">
-      <button type="submit" class="primary">Apply now </button>
-      </form>
       </section>
-  </article>
-
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName" />
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email" />
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
+    </article>
   `,
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
@@ -52,21 +53,17 @@ export class DetailsComponent {
     lastName: new FormControl(''),
     email: new FormControl(''),
   });
-
-  constructor()
-  {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+  constructor() {
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then((housingLocation) => {
       this.housingLocation = housingLocation;
-    })
+    });
   }
-  submitApplication()
-  {
+  submitApplication() {
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',
       this.applyForm.value.lastName ?? '',
-      this.applyForm.value.email ?? ''
-    )
+      this.applyForm.value.email ?? '',
+    );
   }
-
 }
